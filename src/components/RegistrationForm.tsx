a8f38b0e742c50f62a2d772fc4fd4686a994d515
Miftachussurur <miftachussurur1993@gmail.com>
2026-04-25 14:11:15 +0700

    fix: update redirect URL in RegistrationForm to point to the correct homepage

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -266,7 +266,7 @@ TERIMA KASIH TELAH MENDAFTAR!
           <Button onClick={() => window.print()} variant="secondary" className="flex items-center justify-center">
             <Printer className="w-5 h-5 mr-2" /> CETAK
           </Button>
-          <Button onClick={() => window.location.href = 'https://pccpolines.org'} className="sm:col-span-2 group">
+          <Button onClick={() => window.location.href = 'https://web-kehadiran-sertijab.vercel.app/'} className="sm:col-span-2 group">
             KEMBALI KE BERANDA <ArrowRight className="w-5 h-5 ml-2 group-hover:translate-x-1 transition-transform" />
           </Button>
         </div>
